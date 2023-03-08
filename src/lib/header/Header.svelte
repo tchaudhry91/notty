26ca199b89c02a8c23cc431a8536d7cce6f999ce
Tanmay Chaudhry <tchaudhry@tanmays-macbook-air.ratfish-mamba.ts.net>
2023-03-08 14:26:08 +0530

    base setup

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let showNav = false;
+</script>
+
+<div class="sticky top-0 z-50">
+	<!-- Nav Bar -->
+	<div class="navbar bg-primary text-primary-content mx-auto">
+		<div class="flex-1">
+			<a href="/">
+				<i class="fa-solid fa-house" />
+			</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal px-1">
+				<li>
+					<a href="/">Login</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+	import Header from '$lib/header/Header.svelte';
+</script>
+
+<Header />
+
+<slot />
